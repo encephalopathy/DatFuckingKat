a906de279b60957ce2da70d92b9fdf8e094b471e
Andrew Facchiano <andrewfacchiano@gmail.com>
2016-01-30 16:40:50 -0800

    validator
    adding validator logic and terrible things

---
+++ DatFuckingGame/Assets/PhraseValidator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhraseValidator : MonoBehaviour {
+
+	public GameObject[] slots;
+	
+	public void Start () {
+		slots = getSlots();
+
+	}
+
+	public GameObject[] getSlots () {
+		//google for all
+		GameObject[] slots = GameObject.FindGameObjectsWithTag("Slot");
+		Debug.Log(slots[0]);
+		Debug.Log(slots[1]);
+		Debug.Log(slots[2]);
+		Debug.Log(slots[3]);
+
+		return slots;
+
+	}
+
+	public bool isValid() {
+		if (isValidPhrase(slots))
+    	{
+        	return true;
+    	}
+    	return false;
+	}
+
+// I know...I KNOW OK? It's a game jam...and I don't care...STOP JUDGING ME!
+	public bool isValidPhrase(GameObject[] slots) {
+		bool valid = false;
+		if(slots[0].transform.GetComponentInChildren<wordValuesDto>() != null &&
+		slots[1].transform.GetComponentInChildren<wordValuesDto>() != null &&
+		slots[2].transform.GetComponentInChildren<wordValuesDto>() != null &&
+		slots[3].transform.GetComponentInChildren<wordValuesDto>() != null ) {
+			if(slots[3].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "who" &&
+			 slots[2].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "what" && 
+			 slots[1].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "when" && 
+			 slots[0].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "where"  ) {
+				valid = true;
+			}
+			else if (slots[3].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "when" &&
+			 slots[2].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "who" && 
+			 slots[1].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "what" && 
+			 slots[0].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "where"  ) {
+			 	valid = true;
+
+			}
+			else if (slots[3].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "who" &&
+			 slots[2].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "what" && 
+			 slots[1].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "where" && 
+			 slots[0].transform.GetChild(0).GetComponent<wordValuesDto>().wordType == "when"  ) {
+				valid = true;
+			}
+	}
+		return valid;
+	}
+
+}
---
+++ DatFuckingGame/Assets/wordValuesDto.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wordValuesDto : MonoBehaviour {
+
+	public string wordType = "who";
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
