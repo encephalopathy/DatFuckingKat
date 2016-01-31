0fe4ecd9944579fb59dca9a4096574c3a33ccf48
Alec Asperslag <aaspersl@ucsc.edu>
2016-01-31 13:51:36 -0800

    Cat Head

--- DatFuckingGame/Assets/Scripts/TurnToMousePos.cs
+++ DatFuckingGame/Assets/Scripts/TurnToMousePos.cs
@@ -5,6 +5,7 @@ public class TurnToMousePos : MonoBehaviour {
 
     public Camera mouseCam;
     public float zDistance = 1;
+    public float yOffset = -2;
 
 	// Use this for initialization
 	void Start () {
@@ -15,10 +16,15 @@ public class TurnToMousePos : MonoBehaviour {
 	void Update () {
         Vector3 mScreenPos = Input.mousePosition;
         mScreenPos.z = zDistance;
-        Vector3 mWorldPos = mouseCam.ScreenToWorldPoint(mScreenPos);
-        //transform.LookAt(mWorldPos, Vector3.left);
-        Vector3 direction = mWorldPos - transform.position;
-        Quaternion toRot = Quaternion.FromToRotation(transform.up, direction);
-        transform.rotation = Quaternion.Lerp(transform.rotation, toRot, 5*Time.time);
-	}
+        Vector3 mWorldPos = mouseCam.ScreenToWorldPoint(mScreenPos);// + new Vector3(0, yOffset, 0);
+
+        //mWorldPos.y = mWorldPos.y + yOffset;
+        transform.LookAt(mWorldPos, Vector3.up);
+        transform.Rotate(new Vector3(1.0f, 0, 0), 90);
+        transform.Rotate(new Vector3(0, 1, 0), 180);
+        ////transform.LookAt(mWorldPos, Vector3.left);
+        //Vector3 direction = mWorldPos - transform.position;
+        //Quaternion toRot = Quaternion.FromToRotation(transform.up, direction);
+        //transform.localRotation = toRot;// Quaternion.Lerp(transform.localRotation, toRot, 100*Time.time);
+    }
 }
