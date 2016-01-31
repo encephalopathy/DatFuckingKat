b5c8be56659576686c3f80beb0cb89d18444e624
Alec Asperslag <aaspersl@ucsc.edu>
2016-01-31 13:04:58 -0800

    Some smoke and mouse follow

---
+++ DatFuckingGame/Assets/Scripts/TurnToMousePos.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnToMousePos : MonoBehaviour {
+
+    public Camera mouseCam;
+    public float zDistance = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mScreenPos = Input.mousePosition;
+        mScreenPos.z = zDistance;
+        Vector3 mWorldPos = mouseCam.ScreenToWorldPoint(mScreenPos);
+        //transform.LookAt(mWorldPos, Vector3.left);
+        Vector3 direction = mWorldPos - transform.position;
+        Quaternion toRot = Quaternion.FromToRotation(transform.up, direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRot, 5*Time.time);
+	}
+}
